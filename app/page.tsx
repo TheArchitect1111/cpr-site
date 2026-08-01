133d07ec45cd9b7df89f11686899cc3de97b6011
The Architect <brickmail1@gmail.com>
2026-08-01 09:08:41 -0400

    Correct North American fee agreement on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,25 @@ import { getLandingPageConfig } from '@/lib/get-landing-config';
 
 export const dynamic = 'force-dynamic';
 
+const NORTH_AMERICAN_FEE_AGREEMENT =
+  'https://docs.google.com/forms/d/e/1FAIpQLSexOTZti6lP_scn4Igt9wwTmxpA3J2csHYaQ0JMGtTp82Zb5Q/viewform';
+
 export default async function Home() {
   const config = await getLandingPageConfig();
-  return <LandingPage config={config} />;
+  const correctedConfig = {
+    ...config,
+    links: {
+      ...config.links,
+      agreement: NORTH_AMERICAN_FEE_AGREEMENT,
+    },
+    footer: {
+      ...config.footer,
+      resources: config.footer.resources.map((item) =>
+        item.label === 'North America Fee Agreement'
+          ? { ...item, href: NORTH_AMERICAN_FEE_AGREEMENT }
+          : item,
+      ),
+    },
+  };
+  return <LandingPage config={correctedConfig} />;
 }
